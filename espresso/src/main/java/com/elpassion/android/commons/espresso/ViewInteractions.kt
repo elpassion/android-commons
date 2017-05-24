12764adc07649143c652029db49610c20c98f547
Karol Kowalski <ordon1313@gmail.com>
2017-05-24 16:55:40 +0200

    Revert "Add onToolbarBackArrow"
    This reverts commit 7c22f4d3317cfa0b54a1ffdccb48658dc4fb582e.

--- espresso/src/main/java/com/elpassion/android/commons/espresso/ViewInteractions.kt
+++ espresso/src/main/java/com/elpassion/android/commons/espresso/ViewInteractions.kt
@@ -2,10 +2,8 @@ package com.elpassion.android.commons.espresso
 
 import android.support.annotation.IdRes
 import android.support.annotation.StringRes
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
-import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 
@@ -14,5 +12,3 @@ fun onId(@IdRes viewId: Int): ViewInteraction = onView(withId(viewId))
 fun onText(@StringRes textId: Int): ViewInteraction = onView(withText(textId))
 
 fun onText(text: String): ViewInteraction = onView(withText(text))
-
-fun onToolbarBackArrow(): ViewInteraction = Espresso.onView(ViewMatchers.withContentDescription(R.string.abc_action_bar_up_description))
