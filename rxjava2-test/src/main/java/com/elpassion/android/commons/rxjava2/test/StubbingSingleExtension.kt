bb1d2841df65b309addf620f07dfdd47026beccc
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-06-21 14:40:04 +0200

    #59 Add support for io.reactive.Single

---
+++ rxjava2-test/src/main/java/com/elpassion/android/commons/rxjava2/test/StubbingSingleExtension.kt
@@ -0,0 +1,17 @@
+package com.elpassion.android.commons.rxjava2.test
+
+import com.nhaarman.mockito_kotlin.doReturn
+import io.reactivex.Single
+import org.mockito.stubbing.OngoingStubbing
+
+fun <T> OngoingStubbing<Single<T>>.thenNever(): OngoingStubbing<Single<T>> = thenReturn(Single.never())
+
+fun <T> OngoingStubbing<Single<T>>.thenJust(value: T): OngoingStubbing<Single<T>> = thenReturn(Single.just(value))
+
+fun <T> OngoingStubbing<Single<T>>.thenError(exception: Exception): OngoingStubbing<Single<T>> = thenReturn(Single.error(exception))
+
+fun <T> OngoingStubbing<Single<T>>.doReturnJust(value: T) = doReturn(Single.just(value))
+
+fun <T> OngoingStubbing<Single<T>>.doReturnNever() = doReturn(Single.never())
+
+fun <T> OngoingStubbing<Single<T>>.doReturnError(exception: Exception) = doReturn(Single.error(exception))
