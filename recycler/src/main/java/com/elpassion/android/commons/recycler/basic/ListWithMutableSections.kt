9176609b7f8dc43055d2dc85fa6926887d3fc936
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-06-26 17:09:18 +0200

    Apply CR: Follow the map interface contract and remove nullability from ListWithSection contract

--- recycler/src/main/java/com/elpassion/android/commons/recycler/basic/ListWithMutableSections.kt
+++ recycler/src/main/java/com/elpassion/android/commons/recycler/basic/ListWithMutableSections.kt
@@ -1,5 +1,5 @@
 package com.elpassion.android.commons.recycler.basic
 
 interface ListWithMutableSections<Item, in Section> : ListWithSections<Item, Section> {
-    override val sections: MutableMap<in Section, out MutableList<Item>?>
+    override val sections: MutableMap<in Section, out MutableList<Item>>
 }
