b459f6bd73813f9f436f625f2ca368cc98d64140
macPry <maciej.przybyl@elpassion.pl>
2017-07-06 17:28:58 +0200

    [refactor] Use better way for finding toolbar back arrow

--- espresso/src/main/java/com/elpassion/android/commons/espresso/ViewInteractions.kt
+++ espresso/src/main/java/com/elpassion/android/commons/espresso/ViewInteractions.kt
@@ -2,12 +2,11 @@ package com.elpassion.android.commons.espresso
 
 import android.support.annotation.IdRes
 import android.support.annotation.StringRes
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
-import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.v7.widget.Toolbar
+import org.hamcrest.CoreMatchers.*
 
 fun onId(@IdRes viewId: Int): ViewInteraction = onView(withId(viewId))
 
@@ -15,4 +14,4 @@ fun onText(@StringRes textId: Int): ViewInteraction = onView(withText(textId))
 
 fun onText(text: String): ViewInteraction = onView(withText(text))
 
-fun onToolbarBackArrow(): ViewInteraction = Espresso.onView(ViewMatchers.withContentDescription(R.string.abc_action_bar_up_description))
+fun onToolbarBackArrow(): ViewInteraction = onView(allOf(withParent(withClassName(`is`(Toolbar::class.java.name))), withContentDescription(containsString("Navigate up"))))
