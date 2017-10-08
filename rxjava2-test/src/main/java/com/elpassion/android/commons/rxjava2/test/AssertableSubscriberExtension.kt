31d04ed6130825e141bd3443f8b1cce56fd8d009
Karol Kowalski <ordon1313@gmail.com>
2017-10-08 20:20:02 +0200

    Add assertLastValue to rxjava2-test

--- rxjava2-test/src/main/java/com/elpassion/android/commons/rxjava2/test/AssertableSubscriberExtension.kt
+++ rxjava2-test/src/main/java/com/elpassion/android/commons/rxjava2/test/AssertableSubscriberExtension.kt
@@ -2,6 +2,7 @@ package com.elpassion.android.commons.rxjava2.test
 
 import io.reactivex.observers.TestObserver
 import junit.framework.Assert.assertTrue
+import org.junit.Assert
 
 fun <T> TestObserver<T>.assertValuesThat(predicate: (T) -> Boolean) {
     assertTrue(values().all(predicate))
@@ -9,4 +10,8 @@ fun <T> TestObserver<T>.assertValuesThat(predicate: (T) -> Boolean) {
 
 fun <T> TestObserver<T>.assertValueThat(predicate: (T) -> Boolean) {
     assertTrue(values().first().run(predicate))
+}
+
+fun <T> TestObserver<T>.assertLastValue(expected: T) {
+    Assert.assertEquals(expected, values().last())
 }
