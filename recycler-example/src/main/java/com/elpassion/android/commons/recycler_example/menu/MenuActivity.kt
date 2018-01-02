6d1b9c34c482ec9633162d6cf8236391953e46c6
ghostbuster91 <kghost0@gmail.com>
2018-01-02 18:03:58 +0100

    Break to long lines

--- recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/menu/MenuActivity.kt
+++ recycler-example/src/main/java/com/elpassion/android/commons/recycler_example/menu/MenuActivity.kt
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.elpassion.android.commons.recycler.adapters.basicAdapterWithLayoutAndBinder
 import com.elpassion.android.commons.recycler_example.R
-import com.elpassion.android.commons.recycler_example.group.*
+import com.elpassion.android.commons.recycler_example.group.BasicContactsListActivity
+import com.elpassion.android.commons.recycler_example.group.BasicMutableRecyclerWithSectionsActivity
+import com.elpassion.android.commons.recycler_example.group.BasicRecyclerWithSectionActivity
 import com.elpassion.android.commons.recycler_example.list.BasicListActivity
 import kotlinx.android.synthetic.main.example_item.view.*
 import kotlinx.android.synthetic.main.recycler_view.*
@@ -19,10 +21,14 @@ class MenuActivity : AppCompatActivity() {
 
         // tag::recycler-basic-adapter-with-layout-and-binder[]
         val examples = listOf(
-                ExampleItem(name = BasicListActivity.DESCRIPTION, onClick = { BasicListActivity.start(this) }),
-                ExampleItem(name = BasicRecyclerWithSectionActivity.DESCRIPTION, onClick = { BasicRecyclerWithSectionActivity.start(this) }),
-                ExampleItem(name = BasicMutableRecyclerWithSectionsActivity.DESCRIPTION, onClick = { BasicMutableRecyclerWithSectionsActivity.start(this) }),
-                ExampleItem(name = BasicContactsListActivity.DESCRIPTION, onClick = { BasicContactsListActivity.start(this) })
+                ExampleItem(name = BasicListActivity.DESCRIPTION,
+                        onClick = { BasicListActivity.start(this) }),
+                ExampleItem(name = BasicRecyclerWithSectionActivity.DESCRIPTION,
+                        onClick = { BasicRecyclerWithSectionActivity.start(this) }),
+                ExampleItem(name = BasicMutableRecyclerWithSectionsActivity.DESCRIPTION,
+                        onClick = { BasicMutableRecyclerWithSectionsActivity.start(this) }),
+                ExampleItem(name = BasicContactsListActivity.DESCRIPTION,
+                        onClick = { BasicContactsListActivity.start(this) })
         )
 
         recyclerView.adapter = basicAdapterWithLayoutAndBinder(examples, R.layout.example_item) { holder, item ->
